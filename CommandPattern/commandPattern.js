7d6f915c25ea99d697f478eb7938d4030c597954
Ericteen <chengteen@yeah.net>
2017-07-17 23:22:22 +0800

    mediator pattern modified

--- CommandPattern/commandPattern.js
+++ CommandPattern/commandPattern.js
@@ -5,31 +5,21 @@
  * @description 命名模式的目标是将方法的调用,请求或者操作封装到一个单独的对象中,给我们酌情执行同时参数化和传递方法调用的能力
  */
 (function() {
-
     var CarManager = {
-
-        // request information
+        /* request information */
         requestInfo: function(model, id) {
-            return "The information for " + model + " with ID " + id + " is foobar";
+            return 'The purchase info for ' + model + ' with ID ' + id + ' is being processed...';
         },
-
-        // purchase the car
+        /* purchase the car */
         buyVehicle: function(model, id) {
-            return "You have successfully purchased Item " + id + ", a " + model;
-        },
-
-        // arrange a viewing
-        arrangeViewing: function(model, id) {
-            return "You have successfully booked a viewing of " + model + " ( " + id + " ) ";
+            return 'You have successfully purchased Item ' + id + ', a ' + model + '.';
         }
     };
+    CarManager.execute = function(commad) {
+        return CarManager[commad.request](commad.model, commad.carID);
+    };
+    var actionA = CarManager.execute({ request: 'requestInfo', model: 'Ford Mondeo', carID: '543434' });
+    console.log(actionA);
+    var actionB = CarManager.execute({ request: 'buyVehicle', model: 'Ford Mondeo', carID: '543434' });
+    console.log(actionB);
 })();
-
-CarManager.execute = function(name) {
-            return CarManager[name] && CarManager[name].apply(CarManager, [].slice.call(arguments, 1));
-        };
-
-CarManager.execute( "arrangeViewing", "Ferrari", "14523" );
-CarManager.execute( "requestInfo", "Ford Mondeo", "54323" );
-CarManager.execute( "requestInfo", "Ford Escort", "34232" );
-CarManager.execute( "buyVehicle", "Ford Escort", "34232" );
--- Mediator/mediator.js
+++ Mediator/mediator.js
@@ -1,78 +1,69 @@
-var validator = {
-    // all available checks
-    types: {},
-    // error messages in the current
-    // validation session
-    messages: [],
-    // current validation config
-    // name: validation type
-    config: {},
-    // the interface method
-    // 'data' is key => value pairs
-    validate: function(data) {
-        var i, msg, type, checker, result_ok;
-        // reset all messages
-        this.messages = [];
-        for (i in data) {
-            if (data.hasOwnProperty(i)) {
-                type = this.config[i];
-                checker = this.types[type];
-                if (!type) {
-                    continue; // no need to validate
-                }
-                if (!checker) { // uh-oh
-                    throw {
-                        name: "ValidationError",
-                        message: "No handler to validate type " + type
-                    };
-                }
-                result_ok = checker.validate(data[i]);
-                if (!result_ok) {
-                    msg = "Invalid value for *" + i + "*, " + checker.instructions;
-                    this.messages.push(msg);
-                }
+/**
+ * 中介者模式
+ * @Author Cheng_Guanghui
+ * @Date   2017-07-17T18:14:28+0800
+ * @description 中介者模式
+ */
+function Player(name) {
+    this.points = 0;
+    this.name = name;
+}
+Player.prototype.play = function() {
+    this.points += 1;
+    mediator.played();
+};
+var scoreboard = {
+    // HTML element to be updated
+    element: document.getElementById('results'),
+    // update the score display
+    update: function(score) {
+        var i, msg = '';
+        for (i in score) {
+            if (score.hasOwnProperty(i)) {
+                msg += '<p><strong>' + i + '<\/strong>: ';
+                msg += score[i];
+                msg += '<\/p>';
             }
         }
-        return this.hasErrors();
-    },
-    // helper
-    hasErrors: function() {
-        return this.messages.length !== 0;
+        this.element.innerHTML = msg;
     }
 };
-// checks for non-empty values
-validator.types.isNonEmpty = {
-    validate: function(value) {
-        return value !== "";
+var mediator = {
+    // all the player
+    players: {},
+    // initialization
+    setup: function() {
+        var players = this.players;
+        players.home = new Player('Home');
+        players.guest = new Player('Guest');
     },
-    instructions: "the value cannot be empty"
-};
-// checks if a value is a number
-validator.types.isNumber = {
-    validate: function(value) {
-        return !isNaN(value);
+    // someone plays, update the score
+    played: function() {
+        var players = this.players,
+            score = {
+                Home: players.home.points,
+                Guest: players.guest.points
+            };
+        scoreboard.update(score);
     },
-    instructions: "the value can only be a valid number, e.g. 1, 3.14 or 2010"
+    // handle user interactions
+    keypress: function(e) {
+        e = e || window.event; // IE
+        if (e.which === 49) { // key "1"
+            mediator.players.home.play();
+            return;
+        }
+        if (e.which === 48) { // key "0"
+            mediator.players.guest.play();
+            return;
+        }
+    }
 };
-// checks if the value contains only letters and numbers
-validator.types.isAlphaNum = {
-    validate: function(value) {
-        return !/[^a-z0-9]/i.test(value);
-    },
-    instructions: "the value can only contain characters and numbers, no special symbols"
-};
-var data = {
-    first_name: "Super",
-    last_name: "Man",
-    age: "unknown",
-    username: "o_O"
-};
-validator.config = {
-    first_name: 'isNonEmpty',
-    age: 'isNumber',
-    username: 'isAlphaNum'
-};
-validator.validate(data);
-if (validator.hasErrors()) {
-    console.log(validator.messages.join("\n"));
-}
+// go!
+mediator.setup();
+window.onkeypress = mediator.keypress;
+// game over in 30 seconds
+setTimeout(function() {
+    window.onkeypress = null;
+    console.log('Game over!');
+}, 30000);
