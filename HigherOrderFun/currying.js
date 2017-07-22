2d53173ae7bb6ddd7db047f582244bd6c0e6ad21
Ericteen <chengteen@yeah.net>
2017-07-22 22:15:20 +0800

    cuttying modified

--- HigherOrderFun/currying.js
+++ HigherOrderFun/currying.js
@@ -12,3 +12,30 @@ var greeter = function(greeting, separator, emphasis, name) {
 var greetHello = curryIt(greeter, "Hello", ", ", ".");
 greetHello("Heidi"); //"Hello, Heidi."
 greetHello("Eddie"); //"Hello, Eddie."
+
+
+// Curryig
+function curry(fn) {
+    var arity = fn.length;
+
+    return (function resolver() {
+        var memory = Array.prototype.slice.call(arguments);
+        return function() {
+            var local = memory.slice(),
+                next;
+            Array.prototype.push.apply(local, arguments);
+            next = local.length >= arity ? fn : resolver;
+            return next.apply(null, local);
+        };
+    }());
+}
+
+
+// Uncurrying
+Function.prototype.uncurrying = function () {
+  var __self = this;
+  return function () {
+    var obj = Array.prototype.shift.call(arguments);
+    return __self.apply(obj, arguments);
+  };
+};
